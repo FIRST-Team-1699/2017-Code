9aa9b6ffdfb824ef7b301763e62492ce247b1bb0
squirlemaster42 <squirlemaster@gmail.com>
2017-01-24 21:50:35 -0500

    No longer extends XboxController

--- test/org/usfirst/team1699/test/simElectronics/SimXboxController.java
+++ test/org/usfirst/team1699/test/simElectronics/SimXboxController.java
@@ -3,17 +3,14 @@ package org.usfirst.team1699.test.simElectronics;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.usfirst.frc.team1699.utils.drive.XboxController;
-
-public class SimXboxController extends XboxController{
+public class SimXboxController{
 	private enum Button {A, B, X, Y, Back, Start, LSButton, RSButton, RBumper, LBumper};
 	private enum State {Pressed, Released};
 	private double rightStickValue;
 	private double leftStickValue;
 	private Map<Button, State> stateMap = new HashMap<>();
 
-	public SimXboxController(int port, double deadband) {
-		super(port, deadband);
+	public SimXboxController() {
 		this.rightStickValue = 0;
 		this.leftStickValue = 0;
 	}
