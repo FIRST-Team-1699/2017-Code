7deebc500fa78a205cba5bef5fbcc2b593999725
John <z_raizer@protonmail.com>
2017-02-14 20:06:21 -0500

    Update Robot.java

--- src/org/usfirst/frc/team1699/robot/Robot.java
+++ src/org/usfirst/frc/team1699/robot/Robot.java
@@ -86,7 +86,7 @@ public class Robot extends IterativeRobot {
     	g = new GearManipulator("gear", 1, appendageController, gearManipulator); //needs actual values (does not have toggle param)
     	b = new BallShooter("shooter", 2, appendageController, shooter); //needs actual values
     	db = new DriveBase("driveBase", 3, driverController, driveLeft1, driveLeft2, driveRight1, driveRight2); //needs actual values
-    	d = new Drive("dive", 4, driverController, driveLeft1, driveLeft2, driveRight1, driveRight2, enc1, enc2); //needs actual values
+    	d = new Drive("drive", 4, driverController, driveLeft1, driveLeft2, driveRight1, driveRight2, enc1, enc2); //needs actual values
     	t = new Turn("turn", 5, driverController, driveLeft1, driveLeft2, driveRight1, driveRight2, enc1, enc2); //needs actual values
     	a = new BallDoor("auger", 6, appendageController, ballDoor); //needs actual values
     	c = new Climber("climber", 7, appendageController, climber1, climber2, comp); //needs actual values (does not have toggle param)
