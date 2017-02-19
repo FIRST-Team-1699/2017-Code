617cd7a13ae8ffe4e017a2f6d15b9564a2f23876
TheCookingCookie <greg_s_l@sbcglobal.net>
2017-02-19 11:29:32 -0500

    Added Agitator

--- src/org/usfirst/frc/team1699/robot/Constants.java
+++ src/org/usfirst/frc/team1699/robot/Constants.java
@@ -19,6 +19,8 @@ public class Constants {
 	public static final int BALL_DOOR_SOLENOID_CLOSE = 1;
 	public static final int GEAR_MANIPULATOR_SOLENOID_OPEN = 2;
 	public static final int GEAR_MANIPULATOR_SOLENOID_CLOSE = 3;
+	public static final int AGITATOR_SOLENOID_OPEN = 0;
+	public static final int AGITATOR_SOLENOID_CLOSE = 1;
 	
 	public static final int ENCODER1_1 = 0;
 	public static final int ENCODER1_2 = 1;
